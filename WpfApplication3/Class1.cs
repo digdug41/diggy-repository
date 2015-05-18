be598670b61340d73dfa30219eac4dd88b01b6a0
digdug41 <digdug41@gmail.com>
2015-05-18 06:43:20 -0500

    added c# files

---
+++ WpfApplication3/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diggyCards
+{
+    class card
+    {
+        public string cardQuestion;
+        public string cardAnswer1;
+        public string cardAnswer2;
+        public string cardAnswer3;
+        public string cardAnswer4;
+        public int cardCorrectAnswerNo;
+
+
+        public card(string question, string answer1, string answer2, string answer3, string answer4, int correctAnswerNo)
+        {
+            cardQuestion = question;
+            cardAnswer1 = answer1;
+            cardAnswer2 = answer2;
+            cardAnswer3 = answer3;
+            cardAnswer4 = answer4;
+            cardCorrectAnswerNo = correctAnswerNo;
+
+        }
+    }
+}
---
+++ WpfApplication3/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using diggyCards;
+using Microsoft.Win32;
+	
+
+namespace diggyQuiz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int cardNo;
+        //dialog strings below
+        string caption = "This is outside the valid card range";
+
+        public int cardCounter(int incrimentAmount)
+        {
+            if (incrimentAmount>0)
+            {
+                cardNo++;
+            }
+            else
+            {
+                cardNo--;
+            }
+            return cardNo;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            lblCardNo.Content = "Card number " + cardCounter(1);
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (cardNo > 1)
+            {
+                lblCardNo.Content = "Card number " + cardCounter(-1);
+            }
+            else
+            {
+                MessageBox.Show(caption);
+            }
+        }
+    }
+}
